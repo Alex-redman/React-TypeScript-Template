ecd7246cf9d645bfba138f3622ae0b145ed6daaf
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2025-11-19 18:13:39 +0200

    React Typescript Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { HomePage } from './pages/HomePage';
+import { PhonesPage } from './pages/PhonesPage';
+import { TabletsPage } from './pages/TabletsPage';
+import { AccessoriesPage } from './pages/AccessoriesPage';
+import { ProductDetailsPage } from './pages/ProductDetailsPage';
+import { FavoritesPage } from './pages/FavoritesPage';
+import { CartPage } from './pages/CartPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { Footer } from './components/Footer';
+import { CartProvider } from './context/CartContext';
+
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
+export function App() {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <CartProvider>
+      <HashRouter>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/phones" element={<PhonesPage />} />
+            <Route path="/tablets" element={<TabletsPage />} />
+            <Route path="/accessories" element={<AccessoriesPage />} />
+            <Route path="/phones/:id" element={<ProductDetailsPage />} />
+            <Route path="/tablets/:id" element={<ProductDetailsPage />} />
+            <Route path="/accessories/:id" element={<ProductDetailsPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </HashRouter>
+    </CartProvider>
   );
-};
+}
